7f4fe535f0e28c9d45d50115df998cdb2254a6dc
Tyler Gerry <122930311+tmgerry@users.noreply.github.com>
2024-08-21 22:32:30 -0600

    Improve Comments on calendar_parser.py

--- calendar_parser.py
+++ calendar_parser.py
@@ -14,27 +14,33 @@ def calendar_utility(window_start: datetime.datetime,window_end: datetime.dateti
     :param output_to_csv: bool, Enable saving output to CSV Default is False
     """
 
+    # Pull data from ical link, convert to text, and push to library object
     data = requests.get(ical)
     calendar_data = data.text
     calendar = icalendar.Calendar.from_ical(calendar_data)
 
 
-    # 
+    # Add headers to CSV file if enabled
     if output_to_csv == True:
         with open("calendar_data.csv", 'a', newline='') as csvfile:
             csv_output = csv.writer(csvfile, delimiter=',')
             csv_output.writerow(['id','summary','start','end','duration'])
 
     total_time = datetime.timedelta() #create an empty datetime variable to increment total time
-    print()
-    # Step through each event stored in the calendar
     count = 0
+    print()
+    
+    # Step through each event stored in the calendar
     for event in calendar.walk('VEVENT'):
+        # Pull data from each event
         summary = event.get("SUMMARY")
         start = event.decoded("DTSTART")
         end = event.decoded("DTEND")
+
+        # Check if event is within window
         if (start < window_end) and (start > window_start):
             duration = end-start
+            # If csv ouput is enabled, write out to file
             if output_to_csv == True:
                 with open("calendar_data.csv", 'a', newline='') as csvfile:
                     csv_output = csv.writer(csvfile, delimiter=',')
@@ -46,8 +52,9 @@ def calendar_utility(window_start: datetime.datetime,window_end: datetime.dateti
             total_time = total_time + duration
             count += 1
         else:
+            # Event isn't within time window given, skip it
             pass
-            # Event isn't within time range given
+            
 
     print()
     print("Total Time Spent", total_time)
